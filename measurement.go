d9d4aabd99844ceadb039a5c8ee70120c67b2d04
Samrat <smaratrm2@gmail.com>
2023-05-01 06:39:35 +0530

    basic functionality and same unit validation created and tested

--- measurement.go
+++ measurement.go
@@ -1,7 +1,130 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main(){
-	fmt.Println("Hello ")
+type LengthMeasurement struct {
+    value float64
+    unit  string
+}
+
+type WeightMeasurement struct {
+    value float64
+    unit  string
+}
+
+type Measurable interface {
+    to(unit string) float64
+}
+
+var lengthMeasurements = map[string]float64{
+    "millimeter": 1,
+    "centimeter": 0.1,
+    "decimeter": 0.01,
+    "inch": 0.03937,
+    "meter": 0.001,
+}
+
+var weightMeasurements = map[string]float64{
+    "milligram": 10000,
+    "gram": 10,
+    "kilogram": 0.01,
+    "pound": 0.022,
+    "ounce": 0.353,
+}
+
+func NewLengthMeasurement(value float64, unit string) Measurable {
+	if value <= 0 {
+		return nil
+	}
+    if _, ok := lengthMeasurements[unit]; ok {
+        return LengthMeasurement{value, unit}
+    }
+    return nil
+}
+
+func NewWeightMeasurement(value float64, unit string) Measurable {
+		if value <= 0 {
+		return nil
+	}
+    if _, ok := weightMeasurements[unit]; ok {
+        return WeightMeasurement{value, unit}
+    }
+    return nil
+}
+
+func (m LengthMeasurement) to(unit string) float64 {
+    if m.unit == unit {
+        return m.value
+    }
+
+    if factor, ok := lengthMeasurements[m.unit]; ok {
+        if factor2, ok := lengthMeasurements[unit]; ok {
+            ratio := factor / factor2
+            return m.value / ratio
+        }
+    }
+    return math.NaN()
+}
+
+func (m WeightMeasurement) to(unit string) float64 {
+    if m.unit == unit {
+        return m.value
+    }
+
+    if factor, ok := weightMeasurements[m.unit]; ok {
+        if factor2, ok := weightMeasurements[unit]; ok {
+			ratio := factor / factor2
+            return m.value / ratio
+        }
+    }
+    return math.NaN()
+}
+
+func areUnitsOfSameType(m1, m2 Measurable) bool {
+	_, isLengthMeasurement1 := m1.(LengthMeasurement)
+	_, isLengthMeasurement2 := m2.(LengthMeasurement)
+	_, isWeightMeasurement1 := m1.(WeightMeasurement)
+	_, isWeightMeasurement2 := m2.(WeightMeasurement)
+
+	if isLengthMeasurement1 && isLengthMeasurement2 {
+		_, ok := lengthMeasurements[m1.(LengthMeasurement).unit]
+		if ok {
+			_, ok := lengthMeasurements[m2.(LengthMeasurement).unit]
+			return ok
+		}
+	} else if isWeightMeasurement1 && isWeightMeasurement2 {
+		_, ok := weightMeasurements[m1.(WeightMeasurement).unit]
+		if ok {
+			_, ok := weightMeasurements[m2.(WeightMeasurement).unit]
+			return ok
+		}
+	}
+	return false
+}
+
+
+
+
+
+func main() {
+    length := NewLengthMeasurement(10, "centimeter")
+    length2 := NewLengthMeasurement(10, "meter")
+    if length == nil {
+        fmt.Println("Invalid length measurement")
+    } else {
+        fmt.Println(length.to("meter"))
+    }
+
+    weight := NewWeightMeasurement(500, "milligram")
+    weight2 := NewWeightMeasurement(500, "kilogram")
+    if weight == nil {
+        fmt.Println("Invalid weight measurement")
+    } else {
+        fmt.Println(weight.to("gram"))
+    }
+	fmt.Println(areUnitsOfSameType(weight , weight2))
+	fmt.Println(areUnitsOfSameType(length , length2))
 }
--- measurement_test.go
+++ measurement_test.go
@@ -1,16 +1,91 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
+func TestLengthMeasurementTo(t *testing.T) {
+    length := NewLengthMeasurement(10, "centimeter")
+
+    if length == nil {
+        t.Error("Expected valid length measurement, got nil")
+    }
+
+    expected := 0.1
+    result := length.to("meter")
+
+    if math.Abs(expected-result) > 0.00001 {
+        t.Errorf("Expected %f, but got %f", expected, result)
+    }
+
+    result = length.to("inch")
+
+    if math.Abs(expected-result) < 0.00001 {
+        t.Errorf("Expected NaN, but got %f", result)
+    }
+}
+
+func TestWeightMeasurementTo(t *testing.T) {
+    weight := NewWeightMeasurement(500, "milligram")
+
+    if weight == nil {
+        t.Error("Expected valid weight measurement, got nil")
+    }
+
+    expected := 0.5
+    result := weight.to("gram")
+
+    if math.Abs(expected-result) > 0.00001 {
+        t.Errorf("Expected %f, but got %f", expected, result)
+    }
+
+    result = weight.to("pound")
+
+    if math.Abs(expected-result) < 0.00001 {
+        t.Errorf("Expected NaN, but got %f", result)
+    }
+}
+
+func TestInvalidMeasurement(t *testing.T) {
+    length := NewLengthMeasurement(-10, "meter")
+
+    if length != nil {
+        t.Error("Expected invalid length measurement, but got valid measurement")
+    }
+
+    weight := NewWeightMeasurement(1000, "unknown")
+
+    if weight != nil {
+        t.Error("Expected invalid weight measurement, but got valid measurement")
+    }
+}
+
+func TestAreUnitsOfSameType(t *testing.T) {
+	// Test length measurements
+	length1 := NewLengthMeasurement(10, "centimeter")
+	length2 := NewLengthMeasurement(5, "meter")
+	if !areUnitsOfSameType(length1, length2) {
+		t.Errorf("Expected true but got false")
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
+
+	length3 := NewLengthMeasurement(10, "centimeter")
+	weight1 := NewWeightMeasurement(500, "milligram")
+	if areUnitsOfSameType(length3, weight1) {
+		t.Errorf("Expected false but got true")
+	}
+
+	// Test weight measurements
+	weight2 := NewWeightMeasurement(100, "gram")
+	weight3 := NewWeightMeasurement(50, "kilogram")
+	if !areUnitsOfSameType(weight2, weight3) {
+		t.Errorf("Expected true but got false")
+	}
+
+	weight4 := NewWeightMeasurement(100, "gram")
+	length4 := NewLengthMeasurement(5, "meter")
+	if areUnitsOfSameType(weight4, length4) {
+		t.Errorf("Expected false but got true")
 	}
 }
+
