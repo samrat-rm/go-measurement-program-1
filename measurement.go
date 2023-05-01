93d195adc1e5f29fb29ca0b70fc2559a82fcbe87
Samrat <smaratrm2@gmail.com>
2023-05-01 20:17:49 +0530

    functions added and tested

--- measurement.go
+++ measurement.go
@@ -2,85 +2,131 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"strconv"
 )
 
 type LengthMeasurement struct {
-    value float64
-    unit  string
+	value float64
+	unit  string
+}
+
+func (m LengthMeasurement) Value() float64 {
+	return m.value
+}
+
+func (m LengthMeasurement) Unit() string {
+	return m.unit
 }
 
 type WeightMeasurement struct {
-    value float64
-    unit  string
+	value float64
+	unit  string
+}
+
+func (m WeightMeasurement) Value() float64 {
+	return m.value
+}
+
+func (m WeightMeasurement) Unit() string {
+	return m.unit
 }
 
 type Measurable interface {
-    to(unit string) float64
+	convertUnit(unit string) float64
+	Value() float64
+	Unit() string
 }
 
 var lengthMeasurements = map[string]float64{
-    "millimeter": 1,
-    "centimeter": 0.1,
-    "decimeter": 0.01,
-    "inch": 0.03937,
-    "meter": 0.001,
+	"millimeter": 1,
+	"centimeter": 0.1,
+	"decimeter":  0.01,
+	"inch":       0.03937,
+	"meter":      0.001,
 }
 
 var weightMeasurements = map[string]float64{
-    "milligram": 10000,
-    "gram": 10,
-    "kilogram": 0.01,
-    "pound": 0.022,
-    "ounce": 0.353,
+	"milligram": 10000,
+	"gram":      10,
+	"kilogram":  0.01,
+	"pound":     0.022,
+	"ounce":     0.353,
 }
 
 func NewLengthMeasurement(value float64, unit string) Measurable {
 	if value <= 0 {
 		return nil
 	}
-    if _, ok := lengthMeasurements[unit]; ok {
-        return LengthMeasurement{value, unit}
-    }
-    return nil
+	if _, ok := lengthMeasurements[unit]; ok {
+		return LengthMeasurement{value, unit}
+	}
+	return nil
 }
 
 func NewWeightMeasurement(value float64, unit string) Measurable {
-		if value <= 0 {
+	if value <= 0 {
 		return nil
 	}
-    if _, ok := weightMeasurements[unit]; ok {
-        return WeightMeasurement{value, unit}
-    }
-    return nil
+	if _, ok := weightMeasurements[unit]; ok {
+		return WeightMeasurement{value, unit}
+	}
+	return nil
 }
 
-func (m LengthMeasurement) to(unit string) float64 {
-    if m.unit == unit {
-        return m.value
-    }
+func (m LengthMeasurement) convertUnit(unit string) float64 {
+	if m.unit == unit {
+		return m.value
+	}
 
-    if factor, ok := lengthMeasurements[m.unit]; ok {
-        if factor2, ok := lengthMeasurements[unit]; ok {
-            ratio := factor / factor2
-            return m.value / ratio
-        }
-    }
-    return math.NaN()
-}
-
-func (m WeightMeasurement) to(unit string) float64 {
-    if m.unit == unit {
-        return m.value
-    }
-
-    if factor, ok := weightMeasurements[m.unit]; ok {
-        if factor2, ok := weightMeasurements[unit]; ok {
+	if factor, ok := lengthMeasurements[m.unit]; ok {
+		if factor2, ok := lengthMeasurements[unit]; ok {
 			ratio := factor / factor2
-            return m.value / ratio
-        }
-    }
-    return math.NaN()
+			result := m.value / ratio
+
+			// Format the float to 2 decimal places
+			formatted := fmt.Sprintf("%.2f", result)
+
+			// Parse the formatted string back into a float64 type
+			parsed, err := strconv.ParseFloat(formatted, 64)
+			if err != nil {
+				log.Printf("Error parsing measurement: %v", err)
+				return 0
+			}
+
+			// Print the result
+			return parsed
+		}
+	}
+	return math.NaN()
+}
+
+func (m WeightMeasurement) convertUnit(unit string) float64 {
+	if m.unit == unit {
+		return m.value
+	}
+
+	if factor, ok := weightMeasurements[m.unit]; ok {
+		if factor2, ok := weightMeasurements[unit]; ok {
+			ratio := factor / factor2
+			result := m.value / ratio
+
+			// Format the float to 2 decimal places
+			formatted := fmt.Sprintf("%.2f", result)
+
+			// Parse the formatted string back into a float64 type
+			parsed, err := strconv.ParseFloat(formatted, 64)
+			if err != nil {
+				log.Printf("Error parsing measurement: %v", err)
+				return 0
+			}
+
+			// Print the result
+			return parsed
+		}
+	}
+	return math.NaN()
 }
 
 func areUnitsOfSameType(m1, m2 Measurable) bool {
@@ -105,26 +151,76 @@ func areUnitsOfSameType(m1, m2 Measurable) bool {
 	return false
 }
 
+func addMeasurements(m1, m2 Measurable) Measurable {
+	if !areUnitsOfSameType(m1, m2) {
+		return nil
+	}
 
+	switch m1.(type) {
+	case LengthMeasurement:
+		m1Length := m1.(LengthMeasurement)
+		m2InM1Unit := NewLengthMeasurement(m2.convertUnit(m1Length.unit), m1Length.unit)
+		newValue := m1Length.value + m2InM1Unit.convertUnit(m1Length.unit) // use convertUnit here
+		return NewLengthMeasurement(truncate(newValue), m1Length.unit)
 
+	case WeightMeasurement:
+		m1Weight := m1.(WeightMeasurement)
+		m2InM1Unit := NewWeightMeasurement(m2.convertUnit(m1Weight.unit), m1Weight.unit)
+		newValue := m1Weight.value + m2InM1Unit.convertUnit(m1Weight.unit) // use convertUnit here
+		fmt.Println(truncate((newValue)))
+		return NewWeightMeasurement(truncate(newValue), m1Weight.unit)
 
+	default:
+		return nil
+	}
+}
+
+func diffBetweenMeasurements(m1, m2 Measurable) Measurable {
+	if !areUnitsOfSameType(m1, m2) {
+		return nil
+	}
+
+	switch m1.(type) {
+	case LengthMeasurement:
+		m1Length := m1.(LengthMeasurement)
+		m2InM1Unit := NewLengthMeasurement(m2.convertUnit(m1Length.unit), m1Length.unit)
+		newValue := math.Abs(m1Length.value - m2InM1Unit.convertUnit(m1Length.unit)) // use convertUnit here
+		return NewLengthMeasurement(truncate(newValue), m1Length.unit)
+
+	case WeightMeasurement:
+		m1Weight := m1.(WeightMeasurement)
+		m2InM1Unit := NewWeightMeasurement(m2.convertUnit(m1Weight.unit), m1Weight.unit)
+		newValue := math.Abs(m1Weight.value - m2InM1Unit.convertUnit(m1Weight.unit)) // use convertUnit here
+		return NewWeightMeasurement(truncate(newValue), m1Weight.unit)
+
+	default:
+		return nil
+	}
+}
+
+func truncate(value float64) float64 {
+	return math.Round(value*100) / 100
+}
 
 func main() {
-    length := NewLengthMeasurement(10, "centimeter")
-    length2 := NewLengthMeasurement(10, "meter")
-    if length == nil {
-        fmt.Println("Invalid length measurement")
-    } else {
-        fmt.Println(length.to("meter"))
-    }
+	length := NewLengthMeasurement(10, "centimeter")
+	length2 := NewLengthMeasurement(10, "meter")
+	if length == nil {
+		fmt.Println("Invalid length measurement")
+	} else {
+		fmt.Println(length.convertUnit("meter"))
+	}
 
-    weight := NewWeightMeasurement(500, "milligram")
-    weight2 := NewWeightMeasurement(500, "kilogram")
-    if weight == nil {
-        fmt.Println("Invalid weight measurement")
-    } else {
-        fmt.Println(weight.to("gram"))
-    }
-	fmt.Println(areUnitsOfSameType(weight , weight2))
-	fmt.Println(areUnitsOfSameType(length , length2))
+	weight := NewWeightMeasurement(500, "milligram")
+	weight2 := NewWeightMeasurement(500, "gram")
+	if weight == nil {
+		fmt.Println("Invalid weight measurement")
+	} else {
+		fmt.Println(weight.convertUnit("gram"))
+	}
+
+	fmt.Println(addMeasurements(weight, weight2))
+	fmt.Println(addMeasurements(length, length2))
+	fmt.Println(diffBetweenMeasurements(weight, weight2))
+	fmt.Println(diffBetweenMeasurements(length, length2))
 }
--- measurement_test.go
+++ measurement_test.go
@@ -6,59 +6,59 @@ import (
 )
 
 func TestLengthMeasurementTo(t *testing.T) {
-    length := NewLengthMeasurement(10, "centimeter")
+	length := NewLengthMeasurement(10, "centimeter")
 
-    if length == nil {
-        t.Error("Expected valid length measurement, got nil")
-    }
+	if length == nil {
+		t.Error("Expected valid length measurement, got nil")
+	}
 
-    expected := 0.1
-    result := length.to("meter")
+	expected := 0.1
+	result := length.convertUnit("meter")
 
-    if math.Abs(expected-result) > 0.00001 {
-        t.Errorf("Expected %f, but got %f", expected, result)
-    }
+	if math.Abs(expected-result) > 0.00001 {
+		t.Errorf("Expected %f, but got %f", expected, result)
+	}
 
-    result = length.to("inch")
+	result = length.convertUnit("inch")
 
-    if math.Abs(expected-result) < 0.00001 {
-        t.Errorf("Expected NaN, but got %f", result)
-    }
+	if math.Abs(expected-result) < 0.00001 {
+		t.Errorf("Expected NaN, but got %f", result)
+	}
 }
 
 func TestWeightMeasurementTo(t *testing.T) {
-    weight := NewWeightMeasurement(500, "milligram")
+	weight := NewWeightMeasurement(500, "milligram")
 
-    if weight == nil {
-        t.Error("Expected valid weight measurement, got nil")
-    }
+	if weight == nil {
+		t.Error("Expected valid weight measurement, got nil")
+	}
 
-    expected := 0.5
-    result := weight.to("gram")
+	expected := 0.5
+	result := weight.convertUnit("gram")
 
-    if math.Abs(expected-result) > 0.00001 {
-        t.Errorf("Expected %f, but got %f", expected, result)
-    }
+	if math.Abs(expected-result) > 0.00001 {
+		t.Errorf("Expected %f, but got %f", expected, result)
+	}
 
-    result = weight.to("pound")
+	result = weight.convertUnit("pound")
 
-    if math.Abs(expected-result) < 0.00001 {
-        t.Errorf("Expected NaN, but got %f", result)
-    }
+	if math.Abs(expected-result) < 0.00001 {
+		t.Errorf("Expected NaN, but got %f", result)
+	}
 }
 
 func TestInvalidMeasurement(t *testing.T) {
-    length := NewLengthMeasurement(-10, "meter")
+	length := NewLengthMeasurement(-10, "meter")
 
-    if length != nil {
-        t.Error("Expected invalid length measurement, but got valid measurement")
-    }
+	if length != nil {
+		t.Error("Expected invalid length measurement, but got valid measurement")
+	}
 
-    weight := NewWeightMeasurement(1000, "unknown")
+	weight := NewWeightMeasurement(1000, "unknown")
 
-    if weight != nil {
-        t.Error("Expected invalid weight measurement, but got valid measurement")
-    }
+	if weight != nil {
+		t.Error("Expected invalid weight measurement, but got valid measurement")
+	}
 }
 
 func TestAreUnitsOfSameType(t *testing.T) {
@@ -88,4 +88,55 @@ func TestAreUnitsOfSameType(t *testing.T) {
 		t.Errorf("Expected false but got true")
 	}
 }
+func TestDiffBetweenMeasurements(t *testing.T) {
+	// Test valid case for WeightMeasurement
+	weight := NewWeightMeasurement(500, "milligram")
+	weight2 := NewWeightMeasurement(500, "gram")
+	expectedWeight := NewWeightMeasurement(499500, "milligram")
+	resultWeight := diffBetweenMeasurements(weight, weight2)
+	if resultWeight == nil || resultWeight.Value() != expectedWeight.Value() || resultWeight.Unit() != expectedWeight.Unit() {
+		t.Errorf("Unexpected result for diffBetweenMeasurements with WeightMeasurement inputs. Got: %v, expected: %v", resultWeight, expectedWeight)
+	}
 
+	// Test valid case for LengthMeasurement
+	length := NewLengthMeasurement(10, "centimeter")
+	length2 := NewLengthMeasurement(10, "meter")
+	expectedLength := NewLengthMeasurement(990, "centimeter")
+	resultLength := diffBetweenMeasurements(length, length2)
+	if resultLength == nil || resultLength.Value() != expectedLength.Value() || resultLength.Unit() != expectedLength.Unit() {
+		t.Errorf("Unexpected result for diffBetweenMeasurements with LengthMeasurement inputs. Got: %v, expected: %v", resultLength, expectedLength)
+	}
+
+	// Test invalid case
+	invalidMeasurement := NewLengthMeasurement(5, "candela") // "candela" is not a valid unit for length
+	resultInvalid := diffBetweenMeasurements(length, invalidMeasurement)
+	if resultInvalid != nil {
+		t.Errorf("Unexpected result for diffBetweenMeasurements with invalid inputs. Got: %v, expected: nil", resultInvalid)
+	}
+}
+func TestAddMeasurements(t *testing.T) {
+	// Test valid case for WeightMeasurement
+	weight := NewWeightMeasurement(500, "milligram")
+	weight2 := NewWeightMeasurement(500, "gram")
+	expectedWeight := NewWeightMeasurement(500500, "milligram")
+	resultWeight := addMeasurements(weight, weight2)
+	if resultWeight == nil || resultWeight.Value() != expectedWeight.Value() || resultWeight.Unit() != expectedWeight.Unit() {
+		t.Errorf("Unexpected result for addMeasurements with WeightMeasurement inputs. Got: %v, expected: %v", resultWeight, expectedWeight)
+	}
+
+	// Test valid case for LengthMeasurement
+	length := NewLengthMeasurement(10, "centimeter")
+	length2 := NewLengthMeasurement(10, "meter")
+	expectedLength := NewLengthMeasurement(1010, "centimeter")
+	resultLength := addMeasurements(length, length2)
+	if resultLength == nil || resultLength.Value() != expectedLength.Value() || resultLength.Unit() != expectedLength.Unit() {
+		t.Errorf("Unexpected result for addMeasurements with LengthMeasurement inputs. Got: %v, expected: %v", resultLength, expectedLength)
+	}
+
+	// Test invalid case
+	invalidMeasurement := NewLengthMeasurement(5, "candela") // "candela" is not a valid unit for length
+	resultInvalid := addMeasurements(length, invalidMeasurement)
+	if resultInvalid != nil {
+		t.Errorf("Unexpected result for addMeasurements with invalid inputs. Got: %v, expected: nil", resultInvalid)
+	}
+}
